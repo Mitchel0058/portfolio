2109736f889886b9475c605425e7ff7251e189d2
Mitchel0058 <goed0058@hz.nl>
2022-06-15 11:55:08 +0200

    fixed footer position issue

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,15 +1,21 @@
 window.addEventListener('load', function () {
     footer();
 })
+
+function changeFooterPos() {
+    const footer = document.getElementById("footer");
+    const pageHeight = document.body.offsetHeight;
+    const windowHeight = window.innerHeight;
+    if (pageHeight > windowHeight) {
+        footer.style.top = `${pageHeight + 25}px`;
+    } else {
+        footer.style.top = `${windowHeight}px`;
+    }
+}
+
 function footer() {
     console.log('footer js loaded correctly');
-    let footer = document.getElementById("footer");
-    let pageHeight = document.body.offsetHeight;
-    let windowHeight = window.innerHeight;
-
-    if(pageHeight > windowHeight) {
-        footer.style.top = `${pageHeight}px`;
-    } else {
-        footer.style.top = `${windowHeight.toString()}px`;
-    }
+    changeFooterPos()
+    const resizeObserver = new ResizeObserver(() => changeFooterPos());
+    resizeObserver.observe(document.body);
 }
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <script src="/js/footer.js"></script>
-<footer class="bg-gradient text-center text-lg-start position-absolute w-100" id="footer">
+<footer class="bg-gradient text-center position-absolute text-lg-start w-100" id="footer">
     <div class="container p-4">
         <div class="row">
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
